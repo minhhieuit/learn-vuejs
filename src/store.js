ad900a388c857a4875c116805cf712789e2cc954
Minh Hiếu <info.hieuit@gmail.com>
2019-06-20 01:13:03 +0700

    'Update'

--- src/store.js
+++ src/store.js
@@ -5,12 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    arrTask:[],
+    count:0,
   },
   mutations: {
-
+    ADD_TASK:(state,payload) => {
+      state.arrTask.push(payload);
+    },
+    CLEAR_TASK: (state) => {
+      let taskLength = state.arrTask.length;
+      state.arrTask.splice(0,taskLength);
+    }
   },
   actions: {
-
+    addTask({commit},data){
+      commit('ADD_TASK',data);
+    },
+    clearTask({commit}){
+      commit('CLEAR_TASK');
+    }
+  },
+  getters:{
+    arrTask: state => state.arrTask,
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Vuex DEMO</h1>
+    <input type="text" v-model="title" > <br>
+    <input type="button" value="add" @click="addTaskMethod(title)">
+    <input type="button" value="clear" @click="clearTask()">
+    <br>
+    <span>{{arrTask}}</span>
   </div>
 </template>
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+  name:'todos-list',
+  data(){
+    return{
+      title:'',
+    }
+  },
+  computed:{
+    ...mapGetters(['arrTask'])
+  },
+  methods:{
+    ...mapActions(['addTask','clearTask']),
+    addTaskMethod(title){
+      this.addTask({title} );
+      console.log({title} + " clgt")
+      this.title = '';
+    }
+  }
+}
+</script>
+
